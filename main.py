a8fe4a1a463e6ad287ee7d6a7f6998a472fb65fc
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2024-11-04 20:34:41 +0800

    add requirements, and second script

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ import subprocess, sys, os
 from tqdm import tqdm
 import requests
 
-url = ""
-response = requests.get(url, stream=True)
+urls = ["https://raw.githubusercontent.com/Crazypersonalph/anicli-downloader/refs/heads/main/initiate.ps1",
+        ""]
 
-with open("initiate.ps1", "wb") as handle:
-    for data in tqdm(response.iter_content()):
-        handle.write(data)
+for i in urls:
+    url = i.rsplit('/', 1)[-1]
+    response = requests.get(i, stream=True, verify=True)
+    with open(url, "wb") as handle:
+        for data in tqdm(response.iter_content()):
+            handle.write(data)
 
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
+# Initialise install scoop
 p = subprocess.Popen(["powershell.exe -ExecutionPolicy RemoteSigned -file", 
               f"{get_script_path()}\\initiate.ps1"], 
               stdout=sys.stdout)
 p.communicate()
-print(get_script_path())
+
+
+# Install ani-cli plus deps
+p = subprocess.Popen(["powershell.exe -ExecutionPolicy RemoteSigned -file", 
+                      f"{get_script_path()}\\scoop-install.ps1"], 
+                      stdout=sys.stdout)
+p.communicate()
+
+os.remove("initiate.ps1")
---
+++ scoop-install.ps1
@@ -0,0 +1,13 @@
+scoop bucket add extras
+
+scoop install extras/windows-terminal
+
+scoop install git
+
+scoop bucket add extras
+
+scoop install ani-cli
+
+scoop bucket add extras
+
+scoop install fzf ffmpeg mpv yt-dlp aria2
